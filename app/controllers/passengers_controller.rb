970ff2d52122c6afc8eca05574ca234ddb878385
Brandy Austin <brandy.austin.seattle@gmail.com>
2018-04-03 12:34:15 -0700

    Added functions to passengers_controller

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -1,2 +1,43 @@
 class PassengersController < ApplicationController
+
+  def index
+    @passengers = Passenger.all
+  end
+
+  def new
+    @passenger = Passenger.new
+  end
+
+  def create
+    passenger = Passenger.new(passenger_params)
+    if passenger.save
+      redirect_to passengers_path
+    end
+  end
+
+  def show
+    @passenger = Passenger.find(params[:id])
+  end
+
+  def edit
+    @passenger = Passenger.find(params[:id])
+  end
+
+  def update
+    passenger = Passenger.dind(params[:id])
+    passenger.assign_attributes(passenger_params)
+    if passenger.save
+      redirect_to passenger_path(passenger)
+    end
+  end
+
+  def destroy
+    Passenger.destroy(params[:id])
+  end
+
+  private
+  def passenger_params
+    return params.require(:name, :phone_num)
+  end
+
 end
