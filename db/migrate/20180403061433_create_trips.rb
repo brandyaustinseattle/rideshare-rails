34b74c83461f038cf178868536016c92b3261493
Brandy Austin <brandy.austin.seattle@gmail.com>
2018-04-03 11:49:42 -0700

    Database set up with add reference migration

--- db/migrate/20180403061433_create_trips.rb
+++ db/migrate/20180403061433_create_trips.rb
@@ -1,8 +1,7 @@
 class CreateTrips < ActiveRecord::Migration[5.1]
   def change
     create_table :trips do |t|
-      t.integer :driver_id
-      t.integer :passenger_id
+
       t.date :date
       t.integer :rating
       t.float :cost
