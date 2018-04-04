f0ba7c063b55166c5252b7c4ef440251818ffc76
Brandy Austin <brandy.austin.seattle@gmail.com>
2018-04-03 17:07:49 -0700

    Styling for passengers and trips pages

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,3 +1,15 @@
 class Passenger < ApplicationRecord
   has_many :trips
+
+  def total_cost
+
+    total = 0
+
+    trips.each do |trip|
+      total += trip.cost
+    end
+
+    return total
+  end
+
 end
