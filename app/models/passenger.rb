3bbc70137f249adec94aafc6df23ff85f05f5ce4
Brandy Austin <brandy.austin.seattle@gmail.com>
2018-04-03 18:21:44 -0700

    Added trip details page

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,10 +1,13 @@
 class Passenger < ApplicationRecord
-  has_many :trips
+
+  has_many :trips, -> { order(date: :desc) }
+
+  validates :name, presence: { message: "You need to provide a name" }
+  validates :phone_num, presence: { message: "You need to provide a phone number" }
 
   def total_cost
 
     total = 0
-
     trips.each do |trip|
       total += trip.cost
     end
