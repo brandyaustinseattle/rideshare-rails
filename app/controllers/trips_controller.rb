543221f68b0dfa8bee41ed677933725be14de3b4
Brandy Austin <brandy.austin.seattle@gmail.com>
2018-04-05 15:19:50 -0700

    Working on trip edit button

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -40,7 +40,7 @@ class TripsController < ApplicationController
 
   private
   def trip_params
-    return params.require(:id, :driver_id, :passenger_id, :date, :rating, :cost)
+    return params.require(:trip).permit(:id, :driver_id, :passenger_id, :date, :rating, :cost)
   end
 
 
