c0b89553490b06a04279fc95c1127fb71d4533de
Brenda Rios <riosbrenda@gmail.com>
2018-04-04 19:45:43 -0700

    Refactor drivers controller to use strong params.

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -10,10 +10,7 @@ class DriversController < ApplicationController
   end
 
   def create
-    @driver = Driver.new(
-      name: params[:driver][:name],
-      vin: params[:driver][:vin]
-    )
+    @driver = Driver.new(driver_params)
 
     if @driver.save
       redirect_to drivers_path
@@ -51,9 +48,9 @@ class DriversController < ApplicationController
     redirect_to drivers_path
   end
 
-  # private
-  # def driver_params
-  #   return params.require(:name).permit(:vin)
-  # end
+  private
+  def driver_params
+    return params.require(:driver).permit(:name,:vin)
+  end
 
 end
