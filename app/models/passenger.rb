c0232c5992a589a4036401b268b2bdeafd412829
Brenda Rios <riosbrenda@gmail.com>
2018-04-06 15:43:24 -0700

    Added styling to the form fields.

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -4,6 +4,7 @@ class Passenger < ApplicationRecord
 
   validates :name, presence: { message: "You need to provide a name" }
   validates :phone_num, presence: { message: "You need to provide a phone number" }
+  validates :phone_num, numericality: {message: "Please use only numbers in phone number"}
 
   def total_cost
 
